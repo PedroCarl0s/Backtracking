9d8964d31e9a8f175fd7a2cab8877112f1b04f77
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-04-18 15:48:16 -0300

    alteração nos protótipos

---
+++ Permutacao_2/permutacao.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void permutacao(int n, int size, int *array, int *aux);
+void print_res(int size, int *array);
+//void swap(int *i, int *j);
+void generate(int input, int *array);
