eeba0b3fac2ab9a69cd17cc787c813d72d9a6a0f
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-04-22 09:49:52 -0300

    remoção do vetor aux no protótipo

--- Permutacao_2/permutacao.h
+++ Permutacao_2/permutacao.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void permutacao(int n, int size, int *array, int *aux);
+void permutacao(int n, int size, int *array);
 void print_res(int size, int *array);
 //void swap(int *i, int *j);
 void generate(int input, int *array);
